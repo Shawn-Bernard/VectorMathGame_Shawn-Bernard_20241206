d24521049df6ae6871b9de7b7d6298fef6df8072
Shawn Bernard <ssbernard@outlook.com>
2024-11-26 22:58:35 -0400

    Added Enemy ai,

---
+++ Assets/Sctipts/Enemy.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public GameObject player;
+    public float range; //radius of zone
+    public Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+        if (distance <= range)
+        {
+            enemy.SetDestination(player.transform.position);
+        }
+        else if (enemy.remainingDistance <= enemy.stoppingDistance) // If done moving
+        {
+            //Throwing is my target vector
+            if (Randomtarget(ref target))//if true do this
+            {
+                enemy.SetDestination(target);// using whatever vector it spits out by ref
+            }
+        }
+    }
+    bool Randomtarget(ref Vector3 newtarget)
+    {
+        Vector3 randTarget = Random.insideUnitSphere * range;// Random target inside a sphere unit
+        NavMeshHit hit;
+        //taking randtarget, puting out my hit on mesh with the distance of range and checking all areas on mesh
+        if (NavMesh.SamplePosition(randTarget, out hit, range, NavMesh.AllAreas))
+        {
+            newtarget = hit.position;//making my target 
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Vector3 Direction = (transform.position - player.transform.position);
+        float Distance = Direction.magnitude;
+
+        if (Direction.magnitude > 6)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+
+        }
+        Gizmos.DrawLine(transform.position, player.transform.position);
+    }
+       
+
+}
--- Assets/Sctipts/PlayerController.cs
+++ Assets/Sctipts/PlayerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent Player;
+    public GameObject hitMarker;
+    public GameObject placedMarker;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +19,21 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))//Getting the left click of the mouse
         {
+            
             //When it is clicked getting the position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            //Handing in the position when clicked and spitting where it hit
             if (Physics.Raycast(ray, out hit))
             {
+                //Destroy my placedMarker is it isn't empty
+                if (placedMarker != null)
+                {
+                    Destroy(placedMarker);
+                }
+                //Setting my player to move towards my hit
                 Player.SetDestination(hit.point);
+               placedMarker = Instantiate(hitMarker, hit.point, Quaternion.identity);
             }
         }
     }
